5391e28a71aca3fda7c2d33d3c75ab913b105579
Natale Vinto <nvinto@redhat.com>
2021-09-13 15:39:23 +0200

    Use always namespace in commands

--- .github/script.sh
+++ .github/script.sh
@@ -4,10 +4,10 @@ app_name=$1
 image=$2
 namespace=$3
 
-deploy=`oc get deployment $app_name`
+deploy=`oc get deployment $app_name -n $namespace`
 if [[ "$?" -eq 0 ]]; then
-    oc set image deployment/$app_name $app_name=$image
-    oc rollout restart deployment/$app_name
+    oc set image deployment/$app_name $app_name=$image -n $namespace
+    oc rollout restart deployment/$app_name -n $namespace
 else
     oc new-app $image --name $app_name -n $namespace
 fi
