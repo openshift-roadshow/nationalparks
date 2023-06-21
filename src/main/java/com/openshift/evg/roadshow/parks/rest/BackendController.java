113e3c46ebdb66b23ca24e7808865448c9dee46e
LorenaLara17 <127864628+LorenaLara17@users.noreply.github.com>
2023-06-20 22:40:10 -0500

    Update BackendController.java

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
-        return new Backend("nationalparks","Lorena Priscila Lara", new Coordinates("47.039304", "14.505178"), 4);
+        return new Backend("nationalparks","Lorena Priscila Lara G", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
